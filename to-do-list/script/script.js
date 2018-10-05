560e057f949ea1a48da6f70ae8baffb870859f83
Eloa Paradela <eloparadela@gmail.com>
2018-10-05 19:37:51 -0300

    add de objetos

---
+++ to-do-list/script/script.js
@@ -0,0 +1,70 @@
+const addButton = document.querySelector(".addButton")
+const input = document.querySelector(".inputTask")
+const list = document.querySelector(".toDoList")
+const checkButton = document.querySelector(".checkAllTasks")
+const deleteButton = document.querySelector(".deleteAllTasks")
+const hidden = document.querySelector(".hiddenFooter")
+
+addButton.addEventListener("click", function(e){
+    e.preventDefault()
+    
+    deleteButton.innerHTML = "Excluir tudo"
+
+    const regex = /\w+/ig;
+    if(!regex.test(input.value)){
+        input.focus()
+        return false
+    }
+
+    if (input.value === undefined || input.value === null || input.value === "" || input.value === " "){
+        input.focus()
+        return false
+    }
+
+    const newTask = document.createElement("li")
+    const task = document.createElement("h2")
+    const remove = document.createElement("button")
+
+    hidden.style.display = 'flex'
+
+    newTask.className = "toDoList__task"
+    task.className = "taskName"
+    remove.className = "taskButton"
+
+    newTask.draggable = true
+    task.innerHTML = input.value
+    remove.innerHTML = "x"
+
+    newTask.appendChild(task)
+    newTask.appendChild(remove)
+    list.appendChild(newTask)
+
+    remove.addEventListener("click", function(e){
+        e.preventDefault()
+        newTask.remove()
+    })
+
+    task.addEventListener("click", function(e){
+        e.preventDefault()
+        
+        task.classList.toggle("taskNameChecked")
+    })
+
+    checkButton.addEventListener("click", function(e){
+        e.preventDefault()
+        while (task.classList.contains("taskName")){
+            task.classList.remove("taskName")
+            task.classList.add("taskNameChecked")
+        }
+    })
+
+    deleteButton.addEventListener("click", function(e){
+        e.preventDefault()
+     
+        newTask.remove()
+        hidden.style.display = 'none'
+    })
+    
+    input.focus()
+    input.value = ""
+})
