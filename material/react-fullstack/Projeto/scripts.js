4e30ce498ba193b06510e35cf8784d1779ee2d2b
Eloá Paradela <eloparadela@gmail.com>
2018-10-22 22:19:55 -0300

    usando babel

--- material/react-fullstack/Projeto/scripts.js
+++ material/react-fullstack/Projeto/scripts.js
@@ -1,34 +1,27 @@
 //<a class = "link"  href="/conta"></a>
 
 
-// const linkCriarUmaConta= document.createElement('a');
-// linkCriarUmaConta.className = 'link'
-// linkCriarUmaConta.href = "/conta"
-// linkCriarUmaConta.appendChild(document.createTextNode('Criar uma conta'))
+const linkCriarUmaConta = <a class='link' href='/conta'>Criar uma conta</a>
+// React.createElement('a', {className:'link', 
+// href:'/conta', 
+// children:'Criar uma conta'
+// })
 
-const linkCriarUmaConta = React.createElement('a', {className:'link', 
-href:'/conta', 
-children:'Criar uma conta'
-})
 
-// const fazerLogin= document.createElement('a');
-// fazerLogin.className = 'link'
-// fazerLogin.href = "/login"
-// fazerLogin.appendChild(document.createTextNode('Fazer login'))
-const fazerLogin= React.createElement('a',{className:'link',
-href:'/login',
-children:'Fazer login'
-})
+const fazerLogin= <a class='link' href='/login'>fazer login</a>
+// React.createElement('a',{className:'link',
+// href:'/login',
+// children:'Fazer login'
+// })
 
-// const formulario =document.createElement('form')
-// formulario.appendChild(linkCriarUmaConta)
-// formulario.appendChild(fazerLogin) 
 
-const formularioLogin = React.createElement('form',{
-  children:[linkCriarUmaConta,fazerLogin]
-})
-// const divisaoProjeto = document.getElementById('projeto')
-// divisaoProjeto.appendChild(formulario)
+
+const formularioLogin = (
+  <form>
+  <a class='link' href='/conta'>Criar uma conta</a>
+  <a class='link' href='/login'>fazer login</a>
+  </form>
+)
 
 const divisaoProjeto =document.getElementById('projeto')
 
