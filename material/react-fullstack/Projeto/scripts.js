6be07e7297053721eefc315aa11f6f92c0c260c2
mariliamessias <marilia.oliveira@outlook.com>
2018-10-23 20:11:29 -0300

    criando botao

--- material/react-fullstack/Projeto/scripts.js
+++ material/react-fullstack/Projeto/scripts.js
@@ -1,30 +1,55 @@
 //<a class = "link"  href="/conta"></a>
+function Link(props){
+  return(
+    <a className ="link" href ={props.url}>
+    {props.children}
+    </a>
+  )
+}
 
+function Botao(props){
+  return(
+    <input className="botao" type="submit" value={props.children}/> 
+   
+  )
+}
 
-const linkCriarUmaConta = <a class='link' href='/conta'>Criar uma conta</a>
-// React.createElement('a', {className:'link', 
-// href:'/conta', 
-// children:'Criar uma conta'
-// })
+const paginaLogin = (
+  <main className="pagina-login">
+  <h1> Login</h1>
+  <p>Entre com seu login</p>
 
-
-const fazerLogin= <a class='link' href='/login'>fazer login</a>
-// React.createElement('a',{className:'link',
-// href:'/login',
-// children:'Fazer login'
-// })
-
-
-
-const formularioLogin = (
-  <form>
-    {[linkCriarUmaConta, fazerLogin]}
-  </form>
+  <Link url="/conta"> Criar uma conta</Link>
+  <Botao>Enviar</Botao>
+  </main>
 )
 
 const divisaoProjeto =document.getElementById('projeto')
 
-ReactDOM.render(formularioLogin, divisaoProjeto)
+// const linkCriarUmaConta = <a class='link' href='/conta'>Criar uma conta</a>
+// // React.createElement('a', {className:'link', 
+// // href:'/conta', 
+// // children:'Criar uma conta'
+// // })
+
+
+// const fazerLogin= <a class='link' href='/login'>fazer login</a>
+// // React.createElement('a',{className:'link',
+// // href:'/login',
+// // children:'Fazer login'
+// // })
+
+
+
+// const formularioLogin = (
+//   <form>
+//     {[linkCriarUmaConta, fazerLogin]}
+//   </form>
+// )
+
+// const divisaoProjeto =document.getElementById('projeto')
+
+ ReactDOM.render(paginaLogin, divisaoProjeto)
 
 
 
