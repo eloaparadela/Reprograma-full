1632ffcbfffa965a5445cc6fee801d0383cfb7a4
Eloá paradela <eloparadela@gmail.com>
2018-10-24 21:22:54 -0300

    Organizando pastas

--- material/react-fullstack/Projeto/src/index.js
+++ material/react-fullstack/Projeto/src/index.js
@@ -1,48 +1,15 @@
 //<a class = "link"  href="/conta"></a>
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Link from './componentes/Link/Link'
-import Botao from './componentes/Botao/Botao'
 import './index.css'
+import Login  from './paginas/Login/Login';
 
 
-const paginaLogin = (
-  <main className="pagina-login">
-  <h1> Login</h1>
-  <p>Entre com seu login</p>
-
-  <Link url="/conta"> Criar uma conta</Link>
-  <Link url="/login"> Fazer login</Link>
-  <Botao desabilitado>Enviar</Botao>
-  <Botao>Enviar</Botao>
-  {/* <Botao className="botao-danger">Enviar</Botao> */}
-  </main>
-)
 
 const divisaoProjeto =document.getElementById('projeto')
+const pagina = <Login />
 
-// const linkCriarUmaConta = <a class='link' href='/conta'>Criar uma conta</a>
-// // React.createElement('a', {className:'link', 
-// // href:'/conta', 
-// // children:'Criar uma conta'
-// // })
-
-
-// const fazerLogin= <a class='link' href='/login'>fazer login</a>
-// // React.createElement('a',{className:'link',
-// // href:'/login',
-// // children:'Fazer login'
-// // })
-
-
-
-// const formularioLogin = (
-//   <form>
-//     {[linkCriarUmaConta, fazerLogin]}
-//   </form>
-// )
-//  const divisaoProjeto =document.getElementById('projeto')
- ReactDOM.render(paginaLogin, divisaoProjeto)
+ ReactDOM.render(pagina, divisaoProjeto)
 
 
 
---
+++ material/react-fullstack/Projeto/src/paginas/Login/Login.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Link from '../../componentes/Link/Link'
+import Botao from '../../componentes/Botao/Botao'
+import './Login.css'
+
+function Login() {
+return(
+<main className="login">
+  <h1> Login</h1>
+  <p>Entre com seu login</p>
+
+  <Link url="/conta"> Criar uma conta</Link>
+  <Link url="/login"> Fazer login</Link>
+  <Botao desabilitado>Enviar</Botao>
+  <Botao>Enviar</Botao>
+
+</main>
+)
+}
+
+export default Login
